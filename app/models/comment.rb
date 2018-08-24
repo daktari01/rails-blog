190eb2c99dfac67afc057078bd08d054fc7b2168
James Dindi <dindijjames@gmail.com>
2018-08-25 01:00:09 +0300

    Add validation for the comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  validates :username, presence: true, length: {minimum:3, maximum:20}
+  validates :body, presence: true, length: {minimum:3, maximum:100}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
     has_many :comments
-    validates :title, presence: true, length: {minimum:5, maximum:250}
+    validates :title, presence: true, length: {minimum:5, maximum:100}
+    validates :body, presence: true, length: {minimum:5, maximum:250}
 end
