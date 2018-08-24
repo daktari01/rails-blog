32f416ba7d099c7fbfa2ba4fd949225ab4ce2db0
James Dindi <dindijjames@gmail.com>
2018-08-24 14:05:05 +0300

    Add post, view all posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Default route
+  root 'posts#index', as: 'home'
+
+  # Normal route
+  get 'about' => 'pages#about', as: 'about'
+
+  # Resource
+  resources :posts
 end
